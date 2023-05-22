4ae0ce9a93381aa6fc26b0497989ec33859c1407
clavison.zapelini <clavison.zapelini@convcard.com.br>
2023-05-22 11:38:08 -0300

    Alteração main

--- src/main/java/primeira_aula/Imc.java
+++ src/main/java/primeira_aula/Imc.java
@@ -3,7 +3,8 @@ package primeira_aula;
 public class Imc {
 	
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo");
+		System.out.println("Olá Mundo ");
+		System.out.println("**************");
 	}
 
 }
