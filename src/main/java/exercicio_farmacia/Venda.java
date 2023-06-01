0a3fb9c58d198429460face00bd90f7c37528d3d
clavison.zapelini <clavison.zapelini@convcard.com.br>
2023-06-01 11:36:04 -0300

    Exercício farmácia

---
+++ src/main/java/exercicio_farmacia/Venda.java
@@ -0,0 +1,24 @@
+package exercicio_farmacia;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Venda {
+	
+	private Cliente cliente;
+	private Produto produto;
+	private int quantidade;
+	private String medico;
+	
+	@Override
+	public String toString() {
+		return cliente.getNome() + "-" + produto.getNome()+"-"+(produto.getValor() + quantidade);
+	}
+	
+	public boolean isCliente(Cliente c) {
+		return c.getNome().equalsIgnoreCase(cliente.getNome());
+	}
+
+}
